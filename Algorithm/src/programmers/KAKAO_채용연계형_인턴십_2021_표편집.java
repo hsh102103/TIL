8d76cd1110ae0e7055efa0af00f1421b7d3fc78d
hsh102103 <hsh102103@naver.com>
2022-10-30 17:26:12 +0900

    Iffy: KAKAO_채용연계형_인턴십_2021_표편집
    결국 답을 봤다..
    51번째 라인 while문이 핵심인 것 같은데, StringBuilder.insert의 경우 바이트단위가 아닌 스트링단위로 offset을 정하여 값을 넣어줄 수 있다. 그 점에서 역순으로 되돌리며 값을 채워주기 때문에 원래의 자리에 X를 담아줄 수 있는 것 같다.인데 자료구조를 사용하는 정석적인 풀이를 확인해봐야할듯..

--- Algorithm/src/programmers/KAKAO_채용연계형_인턴십_2021_표편집.java
+++ Algorithm/src/programmers/KAKAO_채용연계형_인턴십_2021_표편집.java
@@ -4,71 +4,55 @@ import java.util.*;
 
 /**
  * 명령어 기반으로 표의 행을 선택, 삭제, 복구
- *
  */
 public class KAKAO_채용연계형_인턴십_2021_표편집 {
 
     public static void main(String[] args) {
         int n = 8;
         int k = 2;
-        String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"};
+        String[] cmd = {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"};
         System.out.println(solution(n, k, cmd));
     }
 
     static private String solution(int n, int k, String[] cmd) {
         String answer = "";
 
-        // 작업하는 표
-        List<Integer> beforeList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            beforeList.add(i);
-        }
-
-        List<Integer> afterList = program(k, beforeList, cmd);
-
-        boolean[] checkArray = new boolean[n];
-        for (Integer value : afterList) {
-            checkArray[value] = true;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if(checkArray[i]) answer+= "O";
-            else answer+="X";
-        }
-
-        return answer;
-    }
-    static class Node{
-        int row,value;
-        public Node(int row,int value){
-            this.row = row;
-            this.value = value;
-        }
-    }
-    static private List<Integer> program(int pointer, List<Integer> list, String[] cmd){
-        // 삭제한 행을 저장할 Stack. 삭제된 행과 값을 저장
-        Stack<Node> stack = new Stack<>();
+        // 삭제한 행을 저장할 Stack
+        Stack<Integer> stack = new Stack<>();
+        int size = n;
 
         for (String s : cmd) {
             char c = s.charAt(0);
             switch (c) {
-                case 'U' -> // 포인터 위로 이동
-                        pointer -= s.charAt(2) - '0';
-                case 'D' -> // 포인터 아래로 이동
-                        pointer += s.charAt(2) - '0';
-                case 'C' -> { // 가리키는 행 제거
-                    Integer value = list.remove(pointer);
-                    stack.add(new Node(pointer, value));
-                    if (pointer == list.size() - 1)
-                        pointer--;
-                }
-                case 'Z' -> { // 삭제 행 복구
-                    Node popNode = stack.pop();
-                    list.add(popNode.row, popNode.value);
+                case 'U': // 포인터 위로 이동
+                    k -= Integer.parseInt(s.substring(2));
+                    break;
+                case 'D': // 포인터 아래로 이동
+                    k += Integer.parseInt(s.substring(2));
+                    break;
+                case 'C': // 가리키는 행 제거
+                    stack.add(k);
+                    size--;
+                    if (k == size) k--;
+                    break;
+                case 'Z': // 삭제 행 복구
+                    int value = stack.pop();
+                    if (value <= k) k++;
+                    size++;
+                    break;
                 }
             }
-        }
 
-        return list;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < size; i++) {
+                sb.append('O');
+            }
+            // 차례로 채워준다
+            while (!stack.isEmpty()) {
+                sb.insert(stack.pop().intValue(), 'X');
+            }
+
+            answer = sb.toString();
+            return answer;
+        }
     }
-}
