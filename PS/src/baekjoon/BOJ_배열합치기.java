fa17c71c05a377187afbe5008f1b9c073b0bf974
hsh102103 <hsh102103@naver.com>
2022-11-24 15:59:10 +0900

    Iffy: BOJ_배열합치기
    정렬되어 주어지는 배열 2개에서 각각 인덱스를 확인하는 변수를 사용하여 결과에 차례대로 담아주는 방식으로 문제풀이
    
    sort

--- PS/src/baekjoon/BOJ_배열합치기.java
+++ PS/src/baekjoon/BOJ_배열합치기.java
@@ -1,24 +1,55 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_배열합치기 {
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        for (int i = 0; i < N; i++) {
-            pq.add(sc.nextInt());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < M; i++) {
-            pq.add(sc.nextInt());
-        }
-        int tmp = pq.size();
-        for (int i = 0; i < tmp; i++) {
-            System.out.print(pq.poll()+" ");
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
+        StringBuilder sb =new StringBuilder();
+
+        int pointer1 = 0;
+        int pointer2 = 0;
+        for (int i = 0; i < N+M; i++) {
+            if (pointer1 >= arr1.length) {
+                sb.append(arr2[pointer2]+" ");
+                pointer2++;
+                continue;
+            }if (pointer2 >= arr2.length) {
+                sb.append(arr1[pointer1]+" ");
+                pointer1++;
+                continue;
+            }
+            if (arr1[pointer1] < arr2[pointer2]) {
+                sb.append(arr1[pointer1]+" ");
+                pointer1++;
+                continue;
+            }
+            if (arr1[pointer1] >= arr2[pointer2]) {
+                sb.append(arr2[pointer2]+" ");
+                pointer2++;
+                continue;
+            }
+        }
+            System.out.println(sb);
     }
 }
