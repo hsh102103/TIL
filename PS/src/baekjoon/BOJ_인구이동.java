2162ea203cddba4e79cdc736c54ff3bc5ab03010
hsh102103 <hsh102103@naver.com>
2022-12-15 21:09:08 +0900

    Success: BOJ_인구이동
    list를 이용해서 이동할 공간들을 넣어주고 탐색이 끝난뒤 이동이 한번이라도 있으면 다시 탐색을 하게했다.
    
    BFS, Simulation

--- PS/src/baekjoon/BOJ_인구이동.java
+++ PS/src/baekjoon/BOJ_인구이동.java
@@ -1,23 +1,19 @@
-package baekjoon;// Don't place your source in a package
+package baekjoon;
+// Don't place your source in a package
+
 import java.util.*;
 import java.lang.*;
 
 // Please name your class Main
-class BOJ_인구이동 {
-    static class Node{
-        int r;
-        int c;
-        int point;
-        public Node(int r, int c, int point){
-            this.r = r;
-            this.c =c ;
-            this.point = point;
-        }
-    }
+public class BOJ_인구이동 {
     static int N, L, R;
     static int[][] map;
     static boolean[][] marked;
-    public static void main (String[] args) throws java.lang.Exception {
+    static List<Node> list;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
@@ -25,58 +21,86 @@ class BOJ_인구이동 {
         R = sc.nextInt();
 
         map = new int[N][N];
-        for(int r = 0; r < N; r++) {
-            for(int c = 0; c < N; c++) {
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
                 map[r][c] = sc.nextInt();
             }
         }
-
-        for(int r = 0; r < N; r++) {
-            for(int c = 0; c < N; c++) {
-                marked = new boolean[N][N];
-                bfs(r,c);
-            }
-        }
-        System.out.println(res);
+        System.out.println(move());
     }
-    static int res = 0;
-    static int[] dr = {-1,1,0,0};
-    static int[] dc = {0,0,-1,1};
-    static void bfs(int r, int c){
+
+    static int move() {
+        int result = 0;
+        while (true) {
+            boolean isMove = false;
+            marked = new boolean[N][N];
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    if (marked[r][c]) continue;
+                    int sum = bfs(r, c);
+                    if (list.size() > 1) {
+                        changePopulation(sum);
+                        isMove = true;
+                    }
+                }
+            }
+            if (!isMove) return result;
+            result++;
+        }
+    }
+
+    static void changePopulation(int sum) {
+        int avg = sum / list.size();
+        for (Node n : list) {
+            map[n.r][n.c] = avg;
+        }
+    }
+
+    static int bfs(int r, int c) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(r,c,map[r][c]));
-        int cnt = 1;
-        int sum = q.peek().point;
-        while(!q.isEmpty()){
+        list = new ArrayList<>();
+
+        q.add(new Node(r, c, map[r][c]));
+        list.add(new Node(r, c, map[r][c]));
+        marked[r][c] = true;
+
+        int sum = map[r][c];
+        while (!q.isEmpty()) {
             Node curr = q.poll();
-            for(int d=0;d<4;d++){
+
+            for (int d = 0; d < 4; d++) {
                 int nr = curr.r + dr[d];
                 int nc = curr.c + dc[d];
-                if(isNotRange(nr,nc)) continue;
-                if(marked[nr][nc]) continue;
-                if(!ConfirmToPopulationDiff(curr.point, map[nr][nc])) continue;
+                if (isNotRange(nr, nc)) continue;
+                if (marked[nr][nc]) continue;
+                if (!ConfirmToPopulationDiff(curr.point, map[nr][nc])) continue;
                 marked[nr][nc] = true;
-                q.add(new Node(nr,nc,map[nr][nc]));
-                cnt++;
+                q.add(new Node(nr, nc, map[nr][nc]));
+                list.add(new Node(nr, nc, map[nr][nc]));
                 sum += map[nr][nc];
             }
         }
-        boolean flag = false;
-        for(int i = 0 ; i<N;i++) {
-            for(int j = 0; j<N; j++) {
-                if(marked[i][j]) {
-                    flag = true;
-                    map[i][j] = sum/cnt;
-                }
-            }
-        }
-        if(flag) res ++;
+        return sum;
     }
-    static boolean ConfirmToPopulationDiff(int curr, int next){
-        int diff = (int)Math.abs(curr-next);
+
+    static boolean ConfirmToPopulationDiff(int curr, int next) {
+        int diff = Math.abs(curr - next);
         return L <= diff && diff <= R;
     }
-    static boolean isNotRange(int r, int c){
-        return r<0 || c<0|| r>=N || c>=N;
+
+    static boolean isNotRange(int r, int c) {
+        return r < 0 || c < 0 || r >= N || c >= N;
+    }
+
+    static class Node {
+        int r;
+        int c;
+        int point;
+
+        public Node(int r, int c, int point) {
+            this.r = r;
+            this.c = c;
+            this.point = point;
+        }
     }
 }
